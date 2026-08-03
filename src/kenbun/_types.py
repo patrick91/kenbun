ffca960156c5e7304e574385c9852119f914a62d
Patrick Arminio <patrick.arminio@gmail.com>
2026-08-03 10:08:59 +0100

    Use NotRequired for symlink metadata

--- src/kenbun/_types.py
+++ src/kenbun/_types.py
@@ -1,4 +1,6 @@
-from typing import Literal, TypeAlias, TypedDict
+from typing import Literal, TypeAlias
+
+from typing_extensions import NotRequired, TypedDict
 
 Ecosystem: TypeAlias = Literal["python", "node"]
 
@@ -7,13 +9,10 @@ class AnalysisHints(TypedDict, total=False):
     script_patterns: list[str]
 
 
-class _RequiredFileEntry(TypedDict):
+class FileEntry(TypedDict):
     path: str
     size: int | None
-
-
-class FileEntry(_RequiredFileEntry, total=False):
-    is_symlink: bool
+    is_symlink: NotRequired[bool]
 
 
 __all__ = ["AnalysisHints", "Ecosystem", "FileEntry"]
